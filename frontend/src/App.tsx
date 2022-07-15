70cec10aacd9bf940b64aaf2ca9b89e9360eebb9
Israel Murat Corrêa <israelmurat@gmail.com>
2022-07-14 22:11:06 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,29 @@
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
+  /* Exemplo: </> (Fragment): serve para retornar mais de 1 componente
   return (
-    </*  Fragment: serve para retornar mais de 1 componente */> 
+    </> 
       <h1>Olá mundo!</h1>
       <NotificationButton />
     </>
+  )*/
+
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
   )
+
 }
 
 export default App
