503a91d117eff74a3b1b95430e8a011788542c31
Israel Murat Corrêa <israelmurat@gmail.com>
2022-07-16 10:48:10 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -14,8 +18,16 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(Pageable pageable, String minDate, String maxDate) {
+		
+		// Busca a data atual com fuso horário do sistema.
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate oneYearAgo = today.minusDays(365);
+		
+		LocalDate min = minDate.equals("") ? oneYearAgo : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(pageable, min, max);
 	}
 	
 }
